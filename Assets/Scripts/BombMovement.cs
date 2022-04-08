5b36406e6dc3f0a5bd1e5235ff6527daa59792b2
Jaime Rivas <jaimerivas81@outlook.com>
2022-04-08 04:05:25 -0700

    level design WIP; sprites implemented WIP; camera rework; vfx for projectiles

--- Assets/Scripts/BombMovement.cs
+++ Assets/Scripts/BombMovement.cs
@@ -9,14 +9,18 @@ public class BombMovement : MonoBehaviour
     [SerializeField] private GameObject plaftorm_type;
     [SerializeField] private float gravity_scale = 5.0f;
     [SerializeField] private float falling_gravity_scale = 15.0f;
+    [SerializeField] private float min = 100f;
+    [SerializeField] private float max = 100f;
     private float strength;
     private float arch;
     private Vector2 dir;
+    private float random_rot;
 
     private bool has_been_thrown = false;
 
     private Rigidbody2D rb2D;
     private PlayerCombat player;
+    private Vector3 rot;
 
     public void BombSettings(float throw_strength, float throw_arch, Vector2 direction, GameObject platform_type = null, PlayerCombat player = null)
     {
@@ -30,12 +34,17 @@ public class BombMovement : MonoBehaviour
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        random_rot = UnityEngine.Random.Range(min, max);
+        rot = new Vector3(0, 0, -random_rot);
+        //Debug.Log(random_rot);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(rot * Time.deltaTime);
+
+        //Debug.Log(transform.rotation.z);
     }
 
     private void FixedUpdate()
@@ -52,7 +61,7 @@ public class BombMovement : MonoBehaviour
             has_been_thrown = true;
         }
 
-        if(rb2D.velocity.y >= 0)
+        if (rb2D.velocity.y >= 0)
         {
             rb2D.gravityScale = gravity_scale;
         }
@@ -65,10 +74,11 @@ public class BombMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("Colliding with " + collision.gameObject.name);
-        if(collision.gameObject.tag != "Player")
+        if (collision.gameObject.tag != "Player")
         {
             if(collision.gameObject.tag == "Gum Drop")
             {
+                transform.rotation = Quaternion.identity;
                 GameObject platform = Instantiate(plaftorm_type, transform.position, transform.rotation);
                 player.AddToPlatformsList(platform);
             }
